2f78a6744cca1d0e0a8e23be42b170c277c12614
imashu001 <imashu001@gmail.com>
2020-10-17 12:07:10 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,134 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Checkbox from "./Components/Checkbox";
+import "./App.css";
+import Application from "./Components/Application";
+import Navbar from "./Components/NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      checked: false,
+    };
+  }
+  handleChange = (e) => {
+    this.setState({
+      checked: !this.state.checked,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="body">
+          <div className="left">
+            <h1>Portugal</h1>
+            <ul>
+              <li>
+                <input
+                  type="checkbox"
+                  checked={this.state.checked}
+                  onClick={this.handleClick}
+                />
+                <label> Aasalya Jayavanti</label>
+              </li>
+              <br />
+              <li>
+                <input
+                  type="checkbox"
+                  checked={this.state.checked}
+                  onClick={this.handleClick}
+                />
+                <label> Luveen lawrence</label>
+                <br />
+              </li>
+              <li>
+                {" "}
+                <input
+                  type="checkbox"
+                  checked={this.state.checked}
+                  onClick={this.handleClick}
+                />
+                <label> Ray Melbourne</label>
+                <br />
+              </li>
+              <li>
+                <input
+                  type="checkbox"
+                  checked={this.state.checked}
+                  onClick={this.handleClick}
+                />
+                <label> cayila Breser</label>
+              </li>
+            </ul>
+            <h1>Nicaragua</h1>
+            <ul>
+              <li>
+                <input
+                  type="checkbox"
+                  checked={this.state.checked}
+                  onClick={this.handleClick}
+                />
+                <label> Devendas Nandi</label>
+                <br />
+              </li>
+              <li>
+                <input
+                  type="checkbox"
+                  checked={this.state.checked}
+                  onClick={this.handleClick}
+                />
+                <label> Obasey Chidy</label>
+                <br />
+              </li>
+              <li>
+                <input
+                  type="checkbox"
+                  name="Xenie"
+                  checked={this.state.checked}
+                  onClick={this.handleClick}
+                />
+                <label> Xenie Dolezelovs</label>
+                <br />
+              </li>
+              <li>
+                <input
+                  type="checkbox"
+                  checked={this.state.checked}
+                  onClick={this.handleClick}
+                />
+                <label> Ezequiel Dengra</label>
+              </li>
+            </ul>
+            <h1>Marshall Islands</h1>
+
+            <ul>
+              <li>
+                <input
+                  type="checkbox"
+                  id="aaron"
+                  onChange={this.handleChange}
+                />
+                <label>Aaron Almaraz</label>
+                <br />
+              </li>
+              <li>
+                <input
+                  type="checkbox"
+                  name="jelena"
+                  onChange={this.handleChange}
+                />
+                <label> Jelena Denisova</label>
+              </li>
+            </ul>
+          </div>
+          <div className="right">GCUHVBNK</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="logo">LOGO</div>
+      <div className="navigator">
+        <div className="home">Home</div>
+        <div className="portfolio">Portfolio</div>
+        <div className="client">Client</div>
+        <div className="getintouch">Get in touch</div>
+      </div>
+    </div>
+  );
+}
+export default Navbar;
